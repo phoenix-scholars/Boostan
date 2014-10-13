d5f850e521867108c3bc6a987fc01dc921d814ac
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2014-10-13 22:39:15 +0330

    build command is added.

--- bin/script/build.sh
+++ bin/script/build.sh
@@ -16,66 +16,62 @@
 # http://dpq.co.ir/licence
 #################################################################################
 #configure
-BUILD_FILE_PATH=$(realpath $0)
-PROJECT_PATH=`dirname "$BUILD_FILE_PATH"`
-PROJECT_FULL_PATH=`dirname "$PROJECT_PATH"`
-PROJECT_NAME=`basename "$PROJECT_FULL_PATH"`
-
-for((i=0;i<107;i++));
-do
-    printf "-"
-done
-printf "\n"
-printf "| %20s | %80s |\n" PROJECT_PATH "$PROJECT_PATH"
-printf "| %20s | %80s |\n" PROJECT_FULL_PATH "$PROJECT_FULL_PATH"
-printf "| %20s | %80s |\n" PROJECT_NAME  "$PROJECT_NAME"
-for((i=0;i<107;i++));
-do
-    printf "-"
-done
-printf "\n"
-
-# Clean project
-rm "$PROJECT_FULL_PATH/output/*.pdf"
-rm "$PROJECT_FULL_PATH/output/*.gz"
 
 #Make Project diagrams
-find "$PROJECT_FULL_PATH/attach" -type f -regex ".*\.\(xmi\)" -print0 | while read -d $'\0' file
-do
-	echo "Processing $file file..."
-    filename=$(basename "$file")
-	directoryname=$(dirname "$file")
-    extension="${filename##*.}"
-    filename="${filename%.*}"
-    echo FILE: "${directoryname}/${filename}.pdf"
-    mkdir "$PROJECT_FULL_PATH/src/image/$filename"
-	umbrello --export svg \
-		--directory "$PROJECT_FULL_PATH/src/image/$filename" \
-		"$file"
-done
+function boostan_build_umbrela() {
+	find "$PROJECT_FULL_PATH/attach" -type f -regex ".*\.\(xmi\)" -print0 | while read -d $'\0' file
+	do
+		echo "Processing $file file..."
+	    filename=$(basename "$file")
+		directoryname=$(dirname "$file")
+	    extension="${filename##*.}"
+	    filename="${filename%.*}"
+	    echo FILE: "${directoryname}/${filename}.pdf"
+	    mkdir "$PROJECT_FULL_PATH/src/image/$filename"
+		umbrello --export svg \
+			--directory "$PROJECT_FULL_PATH/src/image/$filename" \
+			"$file"
+	done
+	return 0;
+}
 
-find "$PROJECT_FULL_PATH/src/image" -type f -regex ".*\.\(svg\)" -print0 | while read -d $'\0' file
-do
-	echo "Processing $file file..."
-    filename=$(basename "$file")
-	directoryname=$(dirname "$file")
-    extension="${filename##*.}"
-    filename="${filename%.*}"
-    echo FILE: "${directoryname}/${filename}.pdf"
-    inkscape --export-pdf="${directoryname}/${filename}.pdf" "$file"
-done
+function boostan_build_svg () {
+	find "$PROJECT_FULL_PATH/src/image" -type f -regex ".*\.\(svg\)" -print0 | while read -d $'\0' file
+	do
+		echo "Processing $file file..."
+	    filename=$(basename "$file")
+		directoryname=$(dirname "$file")
+	    extension="${filename##*.}"
+	    filename="${filename%.*}"
+	    echo FILE: "${directoryname}/${filename}.pdf"
+	    inkscape --export-pdf="${directoryname}/${filename}.pdf" "$file"
+	done
+	return 0;
+}
 
-# Check the boostan styles
-if [ -d "$PROJECT_PATH/boostan" ]; then
-	echo "Boostan styles does not exits?!";
-	exit 1;
-fi
+function boostan_build_src() {
+	# Check the boostan styles
+	if [ ! -d "$BOOSTAN_WRK_DIR/boostan" ]; then
+		echo "Boostan styles does not exits?!";
+		return 1;
+	fi
+	
+	#Make new document
+	cd "$BOOSTAN_WRK_DIR/src"
+	xelatex -interaction=nonstopmode -synctex=-1 main.tex
+	xindy -L persian -C utf8 -I xindy -M main -t main.glg -o main.gls main.glo
+	xelatex -interaction=nonstopmode -synctex=-1 main.tex
+	
+	#Deploy
+	cp main.pdf "$BOOSTAN_WRK_DIR/output/${PROJECT_NAME}.pdf"
+	
+	cd "$BOOSTAN_WRK_DIR"
+	return 0;
+}
 
-#Make new document
-cd "$PROJECT_FULL_PATH/src"
-xelatex -interaction=nonstopmode -synctex=-1 main.tex
-xindy -L persian -C utf8 -I xindy -M main -t main.glg -o main.gls main.glo
-xelatex -interaction=nonstopmode -synctex=-1 main.tex
-
-#Deploy
-cp main.pdf "$PROJECT_PATH/output/${PROJECT_NAME}.pdf"
+function boostan_build(){
+	boostan_build_src
+	#boostan_build_svg
+	#boostan_build_umbrela
+	return 0;
+}
--- bin/script/clean.sh
+++ bin/script/clean.sh
@@ -15,55 +15,11 @@
 # 
 # http://dpq.co.ir/licence
 #################################################################################
+#configure
 
-BOOSTAN_VERSION=branches/0.1.1
-BOOSTAN_REPO_PATH=https://github.com/phoenix-scholars/Boostan
-
-
-BOOSTAN_BIN_DIR=$BOOSTAN_INS_DIR/bin
-BOOSTAN_DOC_DIR=$BOOSTAN_INS_DIR/doc
-BOOSTAN_STY_DIR=$BOOSTAN_INS_DIR
-BOOSTAN_WRK_DIR=$PWD
-BOOSTAN_SRC_DIR=$BOOSTAN_WRK_DIR/src
-BOOSTAN_TMP_DIR=$BOOSTAN_WRK_DIR/tmp
-BOOSTAN_OUT_DIR=$BOOSTAN_WRK_DIR/output
-
-BOOSTAN_COMMAND=
-BOOSTAN_VERBOSE=false
-
-PROJECT_NAME=`basename "$BOOSTAN_WRK_DIR"`
- 
-while getopts ":c:vV" opt; do
-	case $opt in
-		c)
-			BOOSTAN_COMMAND=$OPTARG
-			boostan_log "The command is : %s"  "$OPTARG"
-			;;
-		V)
-			BOOSTAN_VERBOSE=true
-			boostan_log "The verbose mode is active"
-			;;
-		v)
-			BOOSTAN_COMMAND=version
-			boostan_log "The command is set to version"
-			;;
-		\?)
-			echo "Invalid option: -$OPTARG" >&2
-			exit 1
-			;;
-		:)
-			echo "Option -$OPTARG requires an argument." >&2
-			exit 1
-			;;
-	esac
-done
-
-
-
-function boostan_configure_print(){
-	boostan_log_print_header "Here is the configuration table"
-	boostan_log_print BOOSTAN_WRK_DIR "$BOOSTAN_WRK_DIR"
-	boostan_log_print BOOSTAN_STY_DIR "$BOOSTAN_STY_DIR"
-	boostan_log_print PROJECT_NAME  "$PROJECT_NAME"
-	boostan_log_print_footer
+# Clean project
+boostan_clean(){
+	rm "$BOOSTAN_WRK_DIR/output/*.pdf"
+	rm "$BOOSTAN_WRK_DIR/output/*.gz"
+	return 0;
 }
